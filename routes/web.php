f44923d2707d004a384d327a10cc38735d64a139
John Doe <johndoe@email.com>
2024-12-10 01:59:09 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::prefix('products')->controller(ProductController::class)->group(function () {
+    Route::get('/', 'index')->name('products');
+    Route::get('/create', 'create')->name('products.create');
+    Route::get('/edit/{id}', 'edit')->name('products.edit');
+    Route::post('/store', 'store')->name('products.store');
+Route::post('/update/{id}', 'update')->name('products.update');
+Route::get('/show/{id}', 'show')->name('products.show');
+
+Route::get('/', function () {
+    return view('welcome');
+});
